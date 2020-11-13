7a1d059e3ca0a1d28027a63a7346d5c20c511098
Jonas Andreasson <mrmrjonne@gmail.com>
2020-11-13 14:29:54 +0100

    Adding all files

---
+++ Projekt/src/sudokuSolver/SudokuSolver.java
@@ -0,0 +1,15 @@
+package sudokuSolver;
+
+public interface SudokuSolver {
+	   /** Tömmer hela sudokut */
+	   void clear();
+
+	   /** Sätter siffran number på raden row och kolumnen col. */
+	   void setNumber(int row, int col, int number);
+
+	   /** Returnerar siffran på raden row och kolumnen col. */
+	   int getNumber(int row, int col);
+
+	   /** Löser sudokut och returnerar true om sudokut går att lösa. */
+	   boolean solve();
+	}
